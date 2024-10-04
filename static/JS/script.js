29ac9bdb7d73566fec84a1c10518fd2682e77531
Spencerscooper <spencercooper1991@gmail.com>
2024-10-04 17:09:36 +0000

    modified home, ourtea.html

--- static/JS/script.js
+++ static/JS/script.js
@@ -17,6 +17,23 @@
     navMenu.classList.toggle("active");
  })
 
+ let eventSection = document.querySelectorAll('.event-section');
+
+window.onscroll =() =>{
+    eventSection.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+
+      if (top >= offset && top < offset + height){
+        sec.classList.add('show-animate')
+      }
+      else{
+        sec.classList.remove('show-animate')
+      }
+    })
+}
+
 
 //  function showNavbar(){
 //   const navBar = document.querySelector(".Tech")
